2cc73e974f3b29e3f4219b6638d632914ac235fb
a.botirov <a.botirov@rtsb.uz>
2026-06-22 09:11:16 +0500

    second commit

--- src/app/public/header/header.ts
+++ src/app/public/header/header.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [NzButtonModule, NzSelectModule, NzIconModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
